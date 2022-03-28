9e963647ded82eaf0ffaffc8ea8cd8a22b3260e6
cui-qinghe <2177265813@qq.com>
2022-03-28 19:56:49 +0800

    master

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Index from "../views/Index"
+import Index from "../views/Index";
+import Details from "../views/Details"
 
 Vue.use(VueRouter);
 
@@ -9,14 +10,13 @@ const routes = [
         path:"/",component:Index
     },
     {
-        path: "/about",
-        name: "about",
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () =>
-            import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
-    },
+        path:"/details",component:Details
+    }
+    // {
+    //     path: "/about",
+    //     component: () =>
+    //         import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    // },
 ];
 
 const router = new VueRouter({
